6e8d616e82abee298ccd4d58cda2fed289214f80
Pyhtagodzilla <1670671958@qq.com>
2024-12-20 23:22:44 +0800

    改了一点点，加了一些注释

--- src/SoilHumidity_ESP32.cpp
+++ src/SoilHumidity_ESP32.cpp
@@ -2,20 +2,24 @@
 #include <PubSubClient.h>
 #include <stdio.h>
 
+// 函数声明
 void callback(char *topic, byte *payload, unsigned int length);
 int getHumidity();
 
-//pin
+// 针脚定义
 #define enableSensor 1
-#define analogPin 2
+#define analogPin 3 //要用的时候再赋能
 
-// WiFi
-const char *ssid = "俺滴牛牛是钢钻";     // Enter your Wi-Fi name
-const char *password = "cddndldadnnsgz"; // Enter Wi-Fi password
+//
+#define INTERVAL 900 // 这是检测间隔，单位为秒
 
-// MQTT Broker
+// WiFi初始化
+const char *ssid = "俺滴牛牛是钢钻";     // WiFi 的 ssid
+const char *password = "cddndldadnnsgz"; // 密码
+
+// MQTT Broker的相关设置
 const char *mqtt_broker = "pythagodzilla.pw";
-const char *topic = "liuLake/Soil";
+const char *topic = "liuLake/SoilHumiditySensor";
 const char *mqtt_username = "pythagodzilla";
 const char *mqtt_password = "jtbx2mtblj";
 const int mqtt_port = 1883;
@@ -25,9 +29,12 @@ PubSubClient client(espClient);
 
 void setup()
 {
-    // Set software serial baud to 115200;
+    // 初始化引脚
+    pinMode(enableSensor, OUTPUT);
+    pinMode(analogPin, INPUT);
+
     Serial.begin(115200);
-    // Connecting to a WiFi network
+    // 连接WiFi
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -35,14 +42,17 @@ void setup()
         Serial.println("Connecting to WiFi..");
     }
     Serial.println("Connected to the Wi-Fi network");
-    // connecting to a mqtt broker
+
+    // 连接MQTT Broker
     client.setServer(mqtt_broker, mqtt_port);
     client.setCallback(callback);
+
     while (!client.connected())
     {
         String client_id = "liuLake";
         client_id += String(WiFi.macAddress());
         Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+
         if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
         {
             Serial.println("Public EMQX MQTT broker connected");
@@ -54,11 +64,13 @@ void setup()
             delay(2000);
         }
     }
-    // Publish and subscribe
+
+    // 发送上线信息，订阅主题
     client.publish(topic, "Sensor is online!");
     client.subscribe(topic);
 }
 
+// 回调函数
 void callback(char *topic, byte *payload, unsigned int length)
 {
     Serial.print("Message arrived in topic: ");
@@ -68,6 +80,8 @@ void callback(char *topic, byte *payload, unsigned int length)
     {
         Serial.print((char)payload[i]);
     }
+
+    // 在这边放你收到消息的动作
     Serial.println();
     Serial.println("-----------------------");
 }
@@ -78,7 +92,8 @@ void loop()
     int humidity = 0;
     char strHumidity[16];
 
-    if (millis() - pastTime > 15000) {
+    // 每隔INTERVAL秒获取一次土壤湿度
+    if ((millis() - pastTime) / 10 > INTERVAL) {
         humidity = getHumidity();
         sprintf(strHumidity, "Humidity = %d", humidity);
         client.publish(topic, strHumidity);
@@ -87,6 +102,7 @@ void loop()
     client.loop();
 }
 
+//这个函数用来获取土壤湿度，独立出来是因为以后可能玩花的所以就先独立出来了
 int getHumidity(){
     static int humidity = 0;
     digitalWrite(enableSensor, HIGH);
